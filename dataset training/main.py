5457880ff781a43f6c478e9a918ba0c492f72863
Wilbur W <wilburwei.2008@gmail.com>
2025-01-11 09:37:30 -0500

    Begin with flask app creation, experimenting with data extraction from HRRR analysis
    Co-authored-by: degree1313 <degree1313@users.noreply.github.com>
    Co-authored-by: rzhang26 <rzhang26@users.noreply.github.com>

---
+++ dataset training/main.py
@@ -0,0 +1,31 @@
+from herbie import Herbie
+import pandas as pd
+import csv
+
+df = pd.read_csv('wildfiredataset.csv')
+value = df.loc[0, 'AcresBurned']
+print(value)
+loc = (35.4676, -97.5164)
+
+points = pd.DataFrame(
+    {
+        "latitude": [loc[0]],
+        "longitude": [loc[1]],
+        "stid": ["Location"],
+    }
+)
+hour = 0
+model = "2024-05-06 12:00"
+H = Herbie(model, model="hrrr", fxx=hour, product="prs")
+edata = {}
+print(H)
+data = H.xarray("(?:GUST:surface|:TMP:2 m|:RH:2 m)", remove_grib=False)\
+# Extract wind and humidity data
+matched = data[0].herbie.pick_points(points)
+edata["temp"] = ((matched.t2m.values[0] - 273.15) * (9/5)) + 32
+edata["rh"] = (matched.r2.values[0])
+
+# Extract wind gust data
+matched = data[1].herbie.pick_points(points)
+edata["gust"] = (matched.gust.values[0] * 1.15078)
+print(edata)
